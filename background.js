2a2a2d5eae965f705e4750a70e6faf34d4a4095c
benjinhua <benjinhua66@gmail.com>
2022-11-09 11:24:52 +0800

    update：监听请求，发送请求

--- background.js
+++ background.js
@@ -1,12 +1,64 @@
-chrome.runtime.onConnect.addListener(function (port) {
-    const extensionListener = function (message, sender, sendResponse) {
-        if (message.name == '信息头') {
-            alert(message.data);
-        }
-    };
-    port.onMessage.addListener(extensionListener);
- 
-    port.onDisconnect.addListener(function(port) {
-        port.onMessage.removeListener(extensionListener);
-    });
+//background.js
+console.log('background.js');
+
+$.get('http://172.23.50.102:8002/product/ones/taskList?projectUuid=LFsDxNc6UznjG7S7',(res) => {
+    console.log(res);
+})
+var taskList = [];
+
+function sendMessageToContentScript(message, callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		chrome.tabs.sendMessage(tabs[0].id, message, function(response)
+		{
+			if(callback) callback(response);
+		});
+	});
+}
+sendMessageToContentScript({cmd:'test', value:'你好，我是popup！'}, function(response)
+{
+	console.log('来自content的回复：'+response);
+});
+
+
+chrome.runtime.onMessage.addListener(function(senderRequest, sender, sendResponse) {
+    if(senderRequest.fromDevTools && senderRequest.fromDevTools=='request' && senderRequest.content){
+        taskList = JSON.parse(senderRequest.content).data.buckets[0].tasks;
+        chrome.tabs.query({
+            active: true, 
+            currentWindow: true
+        }, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {isGetData: taskList.length > 0}, function(res) {
+                console.log('接收content的回调', res);
+            });
+        });
+    }
+    if(senderRequest.fromContent && senderRequest.fromContent === 'sendAjax') {
+        var projectUuid = senderRequest.projectUuid;
+        var taskData = taskList[senderRequest.index]
+        $.ajax({
+            type: 'POST',
+            url: 'http://172.23.50.102:8002/product/ones/syncTask',
+            data: JSON.stringify({
+                projectUuid,
+                taskData
+            }),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function(res) {
+                if(res.code === '0') {
+                    console.log('同步成功');
+                    chrome.tabs.query({
+                        active: true, 
+                        currentWindow: true
+                    }, function(tabs){
+                        chrome.tabs.sendMessage(tabs[0].id, {isAsyncSuccess: true}, function(res) {
+                            console.log('接收content的回调', res);
+                        });
+                    });
+                }
+            }
+        })
+    }
 });
---
+++ js/content.js
@@ -0,0 +1,29 @@
+//content.js   manifest匹配地址的页面在刷新时会直接执行这里的代码
+$(function() {
+    $("body").append("<div class='phoenix-async-success' style='position: absolute;top: 10%;left: 50%;color: #000;background: #f6ffed;/* display: none; */border: 1px solid #b7eb8f;padding: 8px 15px;'>同步成功</div>")
+    $("body").delegate('.phoenix-async-demand', 'click', function() {
+        chrome.runtime.sendMessage(chrome.runtime.id, {//当页面刷新时发送到bg
+            fromContent: 'sendAjax',
+            projectUuid: $($('.project-name')[0]).text(),
+            index: $(this).attr('data-index')
+        });
+    })
+})
+
+chrome.runtime.onMessage.addListener(function(senderRequest, sender, sendResponse) {
+    console.log('demo已运行', senderRequest);
+    if(senderRequest.isGetData) {
+        $('.phoenix-async-demand').remove();
+        setTimeout(() => {
+            $('.task-item-one-row-margin-left').each((index,item) => {
+                $(item).append("<button style='border: none;background: #5ca5ea;color: #fff;padding: 2px 12px;border-radius: 3px;margin-left: 8px;' data-index='"+index+"' class='phoenix-async-demand'>同步</button>")
+            });
+        },1000)
+    }
+    if(senderRequest.isAsyncSuccess) {
+        $('.phoenix-async-success').show();
+        setTimeout(() => {
+            $('.phoenix-async-success').hide();
+        },1000)
+    }
+});
